144ff2091aa55e6b928b531d8342f7ca6cc5feab
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-13 18:53:40 +0530

    code

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const express = require("express");
 const app = express();
 app.post("/signup",function(req, res){
 
-})
+});
 
 app.post("/signin",function(req, res){
 
-})
+});
 
 app.get("/todo",function(req, res){
 
-})
+});
 
 app.post("/todos",function(req, res){
 
-})
+});
+app.listen(3000);
