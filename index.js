7be6af80bf9ca2e2ebdee3adc31c15a6b553ed74
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-17 21:51:49 +0530

    code

--- index.js
+++ index.js
@@ -1,71 +1,14 @@
-const bcrypt = require("bcrypt");
-const express = require("express");   
-const { UserModel, TodoModel } = require("./db");
-const { auth, JWT_SECRET } = require("./auth");
-const jwt = require("jsonwebtoken");
+const express = require("express");
 const mongoose = require("mongoose");
-const { z } = require("zod");
-
-mongoose.connect("mongodb://localhost:27017/mydatabase"); // Add your MongoDB URI
-
-const app = express();
-app.use(express.json());
-
-// SIGNUP ROUTE
-app.post("/signup", async function(req, res) {
-    const requiredBody = z.object({
-        email: z.string(),
-        password: z.string,
-        name: z.string
-
-
-    })
-    //const parsedData = requiredBody.parse(req.body);
-    const parsedDatawithSuccess = requiredBody.safeParse(req.body);
-
-
-    const { email, password, name } = req.body;
-    if(typeof email !== "string" || email.length < 5 ||!email.includes("@")){
-        res.json({
-            message: "Email incorrect"
-        })
-        return
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 5);
-    console.log("Hashed Password:", hashedPassword);
-
-    await UserModel.create({
-        email: email,
-        password: hashedPassword,
-        name: name
-    });
-
-    res.json({
-        message: "You are signed up"
-    });
-});
-
-// SIGNIN ROUTE
-app.post("/signin", async function(req, res) {
-    const { email, password } = req.body;
-
-    const user = await UserModel.findOne({ email });
-
-    if (!user) {
-        return res.status(403).json({ message: "User not found" });
-    }
-
-    const isPasswordCorrect = await bcrypt.compare(password, user.password);
-
-    if (isPasswordCorrect) {
-        const token = jwt.sign({ id: user._id.toString() }, JWT_SECRET);
-        res.json({ token });
-    } else {
-        res.status(403).json({ message: "Incorrect credentials" });
-    }
-});
-
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
+const { userRouter } = ("./routes/user");
+const { adminRouter } = ("./routes/admin");
+const { courseRouter } = ("./routes/course");
+const app = ("express");
+app.use = (express. json());
+app("api/v1/user", userRouter);
+app("api/v1/admin", adminRouter);
+app("api/v1/course", courseRouter);
+async function main(){
+    await mongoose.connect("")
+    console.log("")
+}
